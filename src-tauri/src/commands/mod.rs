1ba8a1adf9618fe5d336feef346719be248e4293
yoonjaekoo-edu <261703@hongcheon.ms.kr>
2026-05-23 20:43:48 +0900

    빌드 오류 해결

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -7,5 +7,6 @@ pub use compile::compile_and_run;
 pub use compiler_detect::{detect_compiler, install_compiler};
 pub use fs_ops::{
     create_problem_file, ensure_workspace, list_today_files, read_file, write_file,
+    list_directory_recursive, create_file, create_directory, rename_item, delete_item,
 };
 pub use settings::{get_settings, save_settings};
